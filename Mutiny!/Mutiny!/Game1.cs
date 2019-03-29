d8eada8a3c03433eff6af8e3f096ab03cc1e7102
Jesper Lundh <lundhjesper@gmail.com>
2019-03-29 11:56:45 +0100

    Generell struktur, Tile, MapManagere

--- Mutiny!/Mutiny!/Game1.cs
+++ Mutiny!/Mutiny!/Game1.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
@@ -9,6 +14,7 @@ namespace Mutiny_
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+        MapManager mapManager;
 
         public Game1()
         {
@@ -24,6 +30,9 @@ namespace Mutiny_
         protected override void LoadContent()
         {
             spriteBatch = new SpriteBatch(GraphicsDevice);
+            Texture2D islandMap1 = Content.Load<Texture2D>("islandMap1");
+            Texture2D mapSpriteSheet = Content.Load<Texture2D>("mapSpriteSheet");
+            mapManager = new MapManager(islandMap1, mapSpriteSheet);
         }
 
         protected override void UnloadContent()
---
+++ Mutiny!/Mutiny!/MapManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Mutiny_
+{
+    //använd bitmap? hanterar den initiella skapelsen av vår värld och alla gameobjekt
+    // ifall vi använder bitmap så måste vi skriva kod för att läsa av varje pixel, titta på dess färg, och sen använda
+    // det för att ladda in en specific tile/fiende/spelare
+    // man använder inte bitmap, man använder Texture2D, men det borde fungera likadant
+    class MapManager
+    {
+        Texture2D islandMap;
+        Texture2D mapSpriteSheet;
+        public MapManager(Texture2D islandMap, Texture2D mapSpriteSheet)
+        {
+            this.islandMap = islandMap;
+            this.mapSpriteSheet = mapSpriteSheet;
+        }
+
+        public Tile[,] Mapbuilder()
+        {
+
+        }
+    }
+}
