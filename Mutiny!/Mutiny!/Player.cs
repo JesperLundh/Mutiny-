97c9ec0e3333e59f3ad279fa9479510cec4b7425
Jesper Lundh <lundhjesper@gmail.com>
2019-03-29 10:24:22 +0100

    Lägger in klasserna Player och Mapbuilder

---
+++ Mutiny!/Mutiny!/Player.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mutiny_
+{
+    //to-do: behöver en update och en draw metod, behöver en textur, position, etc
+    class Player
+    {
+        
+    }
+}
